63becaf181bdc0974025d29f58764fbabbf117a5
tristynb95 <tristendbayley@gmail.com>
2025-09-01 15:21:37 +0100

    Reverting Pre-REfactoring

---
+++ netlify/functions/config.js
@@ -0,0 +1,14 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      apiKey: process.env.VITE_FIREBASE_API_KEY,
+      authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.VITE_FIREBASE_APP_ID,
+      measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+    }),
+  };
+};
